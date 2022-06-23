ea6b07707af2ecc9050f570916ce5798f2c72d3b
wrafanunes <wrsnunes@gmail.com>
2022-06-23 09:15:30 -0300

    esqueleto do projeto

---
+++ DemoLibrary/Accounts.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary
+{
+    public class Accounts
+    {
+        public EmployeeModel Create (PersonModel model)
+        {
+            EmployeeModel employee = new()
+            {
+                FirstName = model.FirstName,
+                LastName = model.LastName,
+                EmailAddress = $"{model.FirstName[..1]}{model.LastName}@krustykrab.com".ToLower()
+            };
+            return employee;
+        }
+    }
+}
---
+++ SOLID.OCP/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using DemoLibrary;
+
+List<PersonModel> people = new()
+{
+    new PersonModel{ FirstName = "Wilson", LastName = "Nunes"},
+    new PersonModel{ FirstName = "Luis", LastName = "Silva"},
+    new PersonModel{ FirstName = "Natan", LastName = "Aparecido"}
+};
+
+List<EmployeeModel> employees = new();
+Accounts accountsProcessor = new();
+foreach(var person in people)
+{
+    employees.Add(accountsProcessor.Create(person));
+}
+foreach(var employee in employees)
+{
+    Console.WriteLine($"{employee.FirstName} {employee.LastName}: {employee.EmailAddress}");
+}
+
+Console.ReadLine();
+
